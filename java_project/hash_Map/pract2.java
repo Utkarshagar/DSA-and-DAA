5581de3c86e7bd49caa119032f61a9436d7deb1c
Utkarshagar <utkarshagarwalcs22@gmail.com>
2024-09-23 22:54:53 +0530

    hello

---
+++ java_project/hash_Map/pract2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class pract2 {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>();
+        map.put("petrol",50);
+        map.put("disel",60);
+        map.put("kerosine",20);
+        System.out.println(map);
+        
+
+    }
+    
+}
