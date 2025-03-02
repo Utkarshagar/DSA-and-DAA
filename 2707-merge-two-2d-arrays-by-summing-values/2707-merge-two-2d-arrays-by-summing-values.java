05ab7fbc90abdc3c77992cb835f0e9f8849cfbe8
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-03-03 01:18:59 +0530

    Time: 4 ms (39.88%), Space: 45.4 MB (61.62%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,29 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-       
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-
-       
-        for (int[] num : nums1) {
-            map.put(num[0], map.getOrDefault(num[0], 0) + num[1]);
+        TreeMap<Integer,Integer>map=new TreeMap<>();
+        for(int num[]:nums1){
+            map.put(num[0],map.getOrDefault(num[0],0)+num[1]);
         }
-
-     
-        for (int[] num : nums2) {
-            map.put(num[0], map.getOrDefault(num[0], 0) + num[1]);
+        for(int num[]:nums2){
+            map.put(num[0],map.getOrDefault(num[0],0)+num[1]);
         }
+        int arr[][]=new int[map.size()][2];
+        int i=0;
+        for(int a:map.keySet()){
+            arr[i][0]=a;
+            arr[i][1]=map.get(a);
+            i++;
 
-       
-        int[][] result = new int[map.size()][2];
-        int index = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            result[index][0] = entry.getKey();
-            result[index][1] = entry.getValue();
-            index++;
         }
-
-        return result;
+        return arr;
+        
     }
 }
