7479039440ec8d385a9476f2a2552e00759c0102
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-03-05 21:22:58 +0530

    Time: 0 ms (100%), Space: 40.9 MB (16.39%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long coloredCells(int n) {
+        long a=(long)Math.pow(n,2);
+        long b=(long)Math.pow(n-1,2);
+        long y=a+b;
+       
+         return y;
+        
+    }
+}
