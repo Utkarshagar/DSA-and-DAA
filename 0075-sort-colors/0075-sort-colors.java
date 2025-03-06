a1189cf0fd113b16ce170790da883d5d4590ce6b
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-03-06 22:35:19 +0530

    Time: 1 ms (23.06%), Space: 42.2 MB (29.18%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        Arrays.sort(arr);
+        // for(int i=0;i<arr.length-1;i++){
+        //     if(arr[i]>arr[i+1]){
+        //         int temp=arr[i];
+        //         arr[i]=arr[i+1];
+        //         arr[i+1]=temp;
+        //     }
+        // }
+        
+    }
+}
