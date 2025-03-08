bf06a0cd5f610e6862d4543019ec3d1de48f7694
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-03-08 10:04:57 +0530

    Time: 3 ms (17.03%), Space: 41.6 MB (58.45%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -5,10 +5,10 @@ class Solution {
         int z=Integer.MAX_VALUE;
         
         for(int i=0;i<=n-k;i++){
-            int w=0;
+            // int w=0;
         int b=0;
             for(int j=i;j<k+i;j++){
-                if(blocks.charAt(j)=='W') w++;
+                // if(blocks.charAt(j)=='W') w++;
                 if(blocks.charAt(j)=='B') b++;
             }
             if(b>=k){
