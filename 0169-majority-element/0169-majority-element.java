c3e5f87f11a27683172eb69571db77f2bb866431
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-03-19 23:13:28 +0530

    Time: 12 ms (36.16%), Space: 50.2 MB (71.34%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int a:nums){
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        int r=0;
+        int m=0;
+        for(int x:map.keySet()){
+            if(map.get(x)>r){
+                 r=Math.max(r,map.get(x));
+                 m=x;
+
+            }
+           
+
+        }
+        return m ;
+        
+    }
+}
