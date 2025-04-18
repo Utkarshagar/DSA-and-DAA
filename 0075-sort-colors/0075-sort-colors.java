3fafefcdecf3c832cc4742f4531a1de0d5e06880
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-04-18 13:54:29 +0530

    Time: 4 ms (3.89%), Space: 42.4 MB (10.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,32 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // Hashmap<Integer,Integer>map=new HashMap<>();
-        int z=0;
-        int o=0;
-        int t=0;
-        for(int a:nums){
-            if(a==0){
-                z++;
-
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
             }
-            if(a==1){
-                o++;
-            }
-            if(a==2){
-                t++;
-            }
-        }
-        for(int i=0;i<z;i++){
-            nums[i]=0;
-        }
-        for(int i=z;i<o+z;i++){
-            nums[i]=1;
-        }
-        for(int i=o+z;i<t+o+z;i++){
-            nums[i]=2;
         }
         
-
         
     }
 }
