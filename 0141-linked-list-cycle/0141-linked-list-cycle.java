80396c009062d747836beb1924d0a8acf60bbbaf
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-05-25 19:17:04 +0530

    Time: 0 ms (100%), Space: 44.8 MB (28.48%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,14 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode>set=new HashSet<>();
-        ListNode temp=head;
-        while(temp!=null){
-            if(set.contains(temp)) return true;
-            set.add(temp);
-            temp=temp.next;
+        ListNode s=head;
+        ListNode f=head;
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+            if(s==f) return true;
         }
         return false;
-
+        
     }
 }
