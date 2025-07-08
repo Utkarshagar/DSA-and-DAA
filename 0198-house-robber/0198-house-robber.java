b84223db90f2d050ba4be2ad50621890c11c407c
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-07-08 10:19:05 +0530

    Time: 0 ms (100%), Space: 41.4 MB (24.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,39 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n=nums.length;
+        
+//         if(n==1) return nums[0];
+//         if(n==2) return Math.max(nums[0],nums[1]);
+//         int arr[]=new int[n];
+//         arr[0]=nums[0];
+//         arr[1]=Math.max(nums[0],nums[1]);
+//         if(n>=3){
+//             for(int i=2;i<n;i++){
+//                 arr[i]=Math.max( nums[i-1],nums[i]+nums[i-2]);
+              
+
+//             }
+//         }
+//         return nums[n-1];
+        
+//     }
+// }
+
 class Solution {
-    public int rob(int[] arr) {
-        int n=arr.length;
-        if(n==1) return arr[0];
-        if(n==2) return Math.max(arr[0],arr[1]);
-        int dp[]=new int[n];
-        
-        if(n>=3){
-            dp[0]=arr[0];
-            dp[1]=Math.max(arr[0],arr[1]);
-            for(int i=2;i<n;i++){
-                dp[i]=Math.max(arr[i]+dp[i-2],dp[i-1]);
-            }
-            
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        int[] arr = new int[n];
+        arr[0] = nums[0];
+        arr[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            arr[i] = Math.max(arr[i - 1], nums[i] + arr[i - 2]);
         }
-        return dp[n-1];
-        
+
+        return arr[n - 1];
     }
 }
