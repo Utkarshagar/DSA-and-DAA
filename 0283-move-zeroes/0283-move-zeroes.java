35528cb70c4e1bc01505c203666fba7fbd1d9113
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-07-12 11:06:01 +0530

    Time: 181 ms (5.02%), Space: 46.7 MB (8.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,17 +3,17 @@ class Solution {
         int c=0;
         for(int a:nums){
             if(a==0) c++;
-
         }
-        while(c!=0){
-            for(int i=1;i<nums.length;i++){
-                if(nums[i-1]==0 && nums[i]!=0){
-                    int temp=nums[i-1];
-                    nums[i-1]=nums[i];
-                    nums[i]=temp;
-                }
+        int n=nums.length;
+        while(c-->0){
+        for(int i=1;i<n;i++){
+            if(nums[i-1]==0 && nums[i]!=0){
+                int temp=nums[i-1];
+                nums[i-1]=nums[i];
+                nums[i]=temp;
             }
-            c--;
         }
+        }
+        
     }
 }
