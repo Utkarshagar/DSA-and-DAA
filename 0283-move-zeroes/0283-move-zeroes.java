2fb09cb7a880ad744ed18924ccc5030081f96d42
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-09-15 12:34:17 +0530

    Time: 2 ms (79.41%), Space: 46.2 MB (68.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,8 +3,8 @@ class Solution {
         int j=0;
         int i=0;
         while(j<nums.length){
-            if(nums[j]!=0){
-                int temp=nums[i];
+            if(nums[j]!=0){//at that point not equal to 0
+                int temp=nums[i];//and swap where is equal to zero
                 nums[i]=nums[j];
                 nums[j]=temp;
                 i++;
