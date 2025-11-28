909d908c61678ff20994ccfcb3f6db91896f7189
UTKARSH AGARWAL <122153222+Utkarshagar@users.noreply.github.com>
2025-11-28 21:47:16 +0530

    Time: 0 ms (100%), Space: 44.4 MB (8.67%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -14,11 +14,13 @@ class Solution {
         ListNode prev=null;
         while(temp!=null){
             ListNode front=temp.next;
+            
             temp.next=prev;
             prev=temp;
             temp=front;
-
         }
         return prev;
+
+        
     }
 }
